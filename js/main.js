ec9119f8d0cf7bc092e4937b6094b51b996fc131
Galih Aris Saputra <galih.ristra@gmail.com>
2017-07-10 12:39:55 +0700

    Bugfix Native Notification

--- js/main.js
+++ js/main.js
@@ -14,22 +14,24 @@ var app = {
     },
 
     initialize: function() {
+        alert('test');
         var self = this;
         this.store = new MemoryStore(function(){
             this.showAlert('Store Initialized', 'Info');
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
-    
-    showAlert: function (message, title) {
-    if (navigator.notification) {
-        navigator.notification.alert(message, null, title, 'OK');
-    } else {
-        alert(title ? (title + ": " + message) : message);
-    }
-},
+
 
 };
 
 app.initialize();
 
+
+function showAlert(message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }
+}
